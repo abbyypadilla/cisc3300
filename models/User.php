b7eb9e742f051d9ae119624a7ca1e38fb2002dd1
abbyypadilla <apadilla26@fordham.edu>
2024-10-23 16:25:27 -0400

    Updated User.php

--- models/User.php
+++ models/User.php
@@ -1,1 +1,12 @@
-
+class User
+{
+    public function getAllUsers()
+    {
+        return 
+        [
+            ['id' => 1, 'name' => 'Nala'],
+            ['id' => 2, 'name' => 'Bailey'],
+            ['id' => 3, 'name' => 'Brady']
+        ];
+    }
+}
