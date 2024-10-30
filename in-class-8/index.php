b213857fbf989769072f95c88d7512340405f03a
abbyypadilla <apadilla26@fordham.edu>
2024-10-29 21:30:09 -0400

    Added in-class-8

---
+++ in-class-8/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/models/User.php';
+require_once __DIR__ . '/controllers/UserController.php';
+
+use controllers\UserController;
+
+$controller = new UserController();
+$controller->index();
+
